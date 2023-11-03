ed7f433e33d105d987b7429feb2436b741ae21ac
Jinendra Jain <jjinendra3@gmail.com>
2023-11-03 13:26:33 +0530

    fixed checklogin

--- checklogin.php
+++ checklogin.php
@@ -3,22 +3,14 @@ session_start();
 if(isset($_POST['username'])&&isset($_POST['pwd'])){
     $username=$_POST['username'];
     $pwd = $_POST['pwd'];
-
     include "connectDB.php";
-     
-     $sql="SELECT * FROM Users WHERE UserName=:username AND Password = :pwd;";
-     $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(
-        ':username' => $username,
-        ':pwd' => $pwd       
-     ));
-    
-    if($stmt->rowCount()>0){
-        while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
-            $_SESSION['id']=$row['UserID'];
-             }
-        
-        header("Location:index.php");
+    $sql="SELECT Password FROM USERS WHERE UserName='$username';";
+    $result=mysqli_connect($sql);
+    if(!$result || $pwd!=$result){
+        echo "Wrong Username please try again!";
+        die;
+    }
+    header("Location:index.php");
         
     }else{
         echo '<span style="color: red;">Login Fail</span>';
